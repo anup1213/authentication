b89f7e3d9a0e407b017c6b03f068e4720f37fd5d
Anup Kumar Tiwari <anupkumartiwari020@gmail.com>
2026-08-27 22:22:00 +0545

    signup route

--- src/app/index.ts
+++ src/app/index.ts
@@ -1,15 +1,20 @@
 import express from 'express'
 import type {Express} from 'express'
-
+import {authRouter} from './auth/routes'
 
 export function createApplication(): Express{
     const app = express()
+
+    // middlewares
+    app.use(express.json())
+    app.use('/auth', authRouter)
 
 
     // routes
     app.get('/', (req, res) => {
         return res.json({message: 'Welcome to chaicode auth service'})
     })
+
 
     return app
 }
